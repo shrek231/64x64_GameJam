2d2dadeceb02e1608ea01041c794085a04063224
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-07-11 14:33:46 -0700

    Create obstacles.cs

---
+++ obstacles.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacles : MonoBehaviour {
+    void Start() {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
