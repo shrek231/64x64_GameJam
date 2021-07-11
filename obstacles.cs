f7bf7752fedbbaf8788eadf118a258dc42c96e77
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-07-11 15:23:27 -0700

    spawns a car and moves localy down

--- obstacles.cs
+++ obstacles.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class obstacles : MonoBehaviour {
+    public static int randY;
+    public GameObject camera;
+    public GameObject car;
+    public static GameObject spawned_car;
     void Start() {
-        
+        SpawnCar();
     }
-    void Update()
-    {
-        
+    void SpawnCar(){
+        randY = UnityEngine.Random.Range(-3, -6);
+        spawned_car = Instantiate(car, new Vector3(UnityEngine.Random.Range(-8, 8), 7, 0), Quaternion.identity);
+    }
+    void Update() {
+        if (spawned_car != null) {
+            spawned_car.transform.Translate(camera.transform.up* randY * Time.deltaTime);
+        }
     }
 }
