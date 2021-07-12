97f7ed102f8f30da18d207eb9ab767f9da76df86
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-07-12 16:12:02 -0700

    car dose not follow player anymore

--- obstacles.cs
+++ obstacles.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 using System.Threading;
 public class obstacles : MonoBehaviour {
     public static int randY;
-    public Vector3 pos;
+    public float posY;
     void Start() {
         GameObject player = GameObject.Find("player");
         playerController player2 = player.GetComponent<playerController>();
-        pos = player2.pos;
+        posY = player2.pos.y;
         randY = UnityEngine.Random.Range(-3, -6);
     }
     void Update() {
-        transform.Translate(pos* randY * Time.deltaTime);
-
+        transform.Translate(new Vector3(0,posY,0)* randY * Time.deltaTime);
         //delete when gets out of distance
         if (transform.position.y < -6){
             Destroy(gameObject);
