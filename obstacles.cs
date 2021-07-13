df04a13465303fd620c565945f234c219f448632
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-07-12 18:43:10 -0700

    car spawner now works sideways

--- obstacles.cs
+++ obstacles.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
 public class obstacles : MonoBehaviour {
-    public static int randY;
-    public float posY;
+    private static int randY;
     void Start() {
         GameObject player = GameObject.Find("player");
         playerController player2 = player.GetComponent<playerController>();
-        posY = player2.pos.y;
-        randY = UnityEngine.Random.Range(-3, -6);
-    }
-    void Update() {
-        transform.Translate(new Vector3(0,posY,0)* randY * Time.deltaTime);
+        bool pos = player2.car_rot;
+        if (pos == true){
+            randY = UnityEngine.Random.Range(3, 6);
+        } else {
+            randY = UnityEngine.Random.Range(-3, -6);
+        }
+    } void Update() {
+        transform.Translate(new Vector3(0,1,0) * randY * Time.deltaTime);
         //delete when gets out of distance
         if (transform.position.y < -6){
             Destroy(gameObject);
