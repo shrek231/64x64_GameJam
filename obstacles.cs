0e6dc0ea824382ff245e059a6ea562cf3ea5079b
Shrek231 <49622239+shrek231@users.noreply.github.com>
2021-07-12 23:58:51 -0700

    added turning and crashing

--- obstacles.cs
+++ obstacles.cs
@@ -3,21 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
 public class obstacles : MonoBehaviour {
+    private bool pos;
     private int randY;
+    private bool rotated = false;
+    private bool crashed = false;
+    public ParticleSystem smoke;
     void Start() {
         GameObject player = GameObject.Find("player");
         playerController player2 = player.GetComponent<playerController>();
-        bool pos = player2.car_rot;
+        pos = player2.car_rot;
         if (pos == true){
             randY = UnityEngine.Random.Range(3, 6);
         } else {
             randY = UnityEngine.Random.Range(-3, -6);
         }
     } void Update() {
-        transform.Translate(new Vector3(0,1,0) * randY * Time.deltaTime);
+        if (!crashed) {
+            transform.Translate(new Vector3(0, 1, 0) * randY * Time.deltaTime);
+        }
         //delete when gets out of distance
         if (transform.position.y < -6){
             Destroy(gameObject);
+        }
+    } void OnTriggerEnter(Collider other){
+        print("hit");
+        if (other.tag == "car"){
+            crashed = true;
+            smoke.Play();
+        }
+        if (other.tag == "death"){
+            print("hit/w tag");
+            if (rotated == false){
+                //pos = true;
+                randY = UnityEngine.Random.Range(3, 6);
+                rotated = true;
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+            } else {
+                rotated = false;
+                //pos = false;
+                randY = UnityEngine.Random.Range(-3, -6);
+            }
         }
     }
 }
